4c60d8d809fc7c7a711b0963008274a0e8cb2f8b
tabassor <felipe.medin@gmail.com>
2026-08-18 08:06:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* みち — service worker */
-const V = 'michi-b002760674';
+const V = 'michi-98f77e4fd9';
 const ASSETS = ['./', './manifest.webmanifest',
                 './icon-192-v3.png', './icon-512-v3.png',
                 './icon-maskable-192-v3.png', './icon-maskable-512-v3.png',
