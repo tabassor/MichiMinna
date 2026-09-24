f58166f278ede019064d3d5e02d1d91b5bd5abad
tabassor <felipe.medin@gmail.com>
2026-09-24 06:09:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 /* みち — service worker */
-const V = 'michi-6bb9fbeed3';
-const ASSETS = ['./', './manifest.webmanifest',
-                './icon-192-v4.png', './icon-512-v4.png',
-                './icon-maskable-192-v4.png', './icon-maskable-512-v4.png',
-                './apple-touch-icon.png'];
+const V = 'michi-2a75d0fcef';
+const ASSETS = ["./", "./manifest.webmanifest", "./icon-192.763e0885.png", "./icon-512.d6112b92.png", "./icon-maskable-192.c4df9da1.png", "./icon-maskable-512.9ed5431b.png", "./apple-touch-icon.298b71c4.png"];
 
 /* Installation instantanée : aucun téléchargement ne doit retarder l'activation,
    sans quoi Chrome ne propose pas l'installation de l'application. */
